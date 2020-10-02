57a647a1cc95754af5e0e4a659c7e06caf80537d
Joep de Jong <joep@joepdejong.com>
2020-10-02 13:05:20 +0200

    Added i18n

--- web/src/containers/MainComponent.js
+++ web/src/containers/MainComponent.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { ReactSVG } from 'react-svg';
 
 import banner from '../assets/img/owtb-banner.svg';
 import Footer from '../Components/Footer';
 import Home from './Home'
 
-export default class MainComponent extends Component {
-  render() {
-    return (
-      <div className="MainComponent">
-          <div className="Banner">
-          <ReactSVG src={banner} />
-          </div>
-          <Home/>
-          <Footer/>
-      </div>
-    )
-  }
+export default function MainComponent() {
+  return (
+    <div className="MainComponent">
+        <div className="Banner">
+        <ReactSVG src={banner} />
+        </div>
+        <Home/>
+        <Footer/>
+    </div>
+  )
 }
