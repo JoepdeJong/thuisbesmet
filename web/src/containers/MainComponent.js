a2cb7b00840202c66fe2d284b323f4fd9fa6f0ae
Joep de Jong <j.dejong-16@student.tudelft.nl>
2021-01-08 11:30:43 +0100

    Fix Google Analytics

--- web/src/containers/MainComponent.js
+++ web/src/containers/MainComponent.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Route, Switch } from 'react-router-dom';
 import Blog from './Blog';
 
@@ -13,6 +13,13 @@ import CookieConsent from 'react-cookie-consent';
 import ReactGA from 'react-ga';
 
 export default function MainComponent() {
+
+  useEffect(() => {
+    if(getCookie('thuisbesmetConsentCookie')){
+      initializeGoogleAnalytics();
+    }
+  })
+
   return (
     <div className="MainComponent">
         <Header/>
@@ -26,8 +33,7 @@ export default function MainComponent() {
           <Route component={Home}/>
         </Switch>
         <Footer/>
-        <CookieConsent location="bottom" cookieName="thuisbesmetConsentCookie" onAccept={() => { ReactGA.initialize('UA-186551147')
-      ReactGA.pageview(window.location.pathname + window.location.search); }} enableDeclineButton
+        <CookieConsent location="bottom" cookieName="thuisbesmetConsentCookie" onAccept={initializeGoogleAnalytics} enableDeclineButton
       onDecline={() => {
         console.log("Cookies have been disabled")
       }} expires={999}>
@@ -35,4 +41,15 @@ export default function MainComponent() {
         </CookieConsent>
     </div>
   )
+}
+
+const getCookie = (name) => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+const initializeGoogleAnalytics = () => { 
+  ReactGA.initialize('G-LEN6YX0T9C')
+  ReactGA.pageview(window.location.pathname + window.location.search); 
 }
